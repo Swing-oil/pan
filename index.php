05d9079e79ecc9231a09d99cbea7bbbd7c087135
Swing-oil <44920669+Swing-oil@users.noreply.github.com>
2019-03-16 10:46:33 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 	<u>油油的盘</u>
 </h1>
 <p>
-	<u><a href="ssh.rar" target="_blank">ssr.rar</a><br />
+	<u><a href="/ssh.rar" target="_blank">/ssr.rar</a><br />
 </u>
 </p>
 <p>
-	<u><u><a href="opera" target="_blank">/opera.exe</a></u><br />
+	<u><u><a href="/opera.exe" target="_blank">/opera.exe</a></u><br />
 </u>
 </p>
