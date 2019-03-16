bca92b73fe5828fac0c0c425dd84ce42b5131c48
Swing-oil <44920669+Swing-oil@users.noreply.github.com>
2019-03-16 10:45:51 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <h1>
-	<u>油油的网盘</u>
+	<u>娌规补鐨勭洏</u>
 </h1>
 <p>
-	<u><a href="/ssh.rar" target="_blank">代理包</a><br />
+	<u><a href="ssh.rar" target="_blank">ssr.rar</a><br />
 </u>
 </p>
 <p>
-	<u><u><a href="/opera" target="_blank">/opera.exe</a></u><br />
+	<u><u><a href="opera" target="_blank">/opera.exe</a></u><br />
 </u>
 </p>
