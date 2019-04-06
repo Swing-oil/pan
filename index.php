1cd7816af30eee95ee8954a1bf02df89318c1e41
Swing-oil <44920669+Swing-oil@users.noreply.github.com>
2019-04-06 09:47:00 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
 	<u>油油的盘</u>
 </h1>
 <p>
+	私有云：使用Ctrl+F快速搜索</p>
+<p>
+	请使用HTTPS+IDM多线程下载</p>
+<p>
 	<u><a href="/IDM-v6.28.15.zip" target="_blank">/IDM-v6.28.15.zip</a><br />
 </u>
 </p>
@@ -53,7 +57,50 @@
 	<u><u><a href="/ImageEye.exe" target="_blank">/ImageEye.exe</a></u><br />
 </u>
 </p>
+
 <p>
-	<u><u><a href="/ImageEye.exe" target="_blank">/ImageEye.exe</a></u><br />
+	<u><u><a href="/Minithunder.exe" target="_blank">/Minithunder.exe</a></u><br />
+</u>
+</p>
+
+<p>
+	<u><u><a href="/ThunderSpeed1.0.35.366.exe" target="_blank">/ThunderSpeed1.0.35.366.exe</a></u><br />
+</u>
+</p>
+<p>
+	<u><u><a href="/GoldWave.rar" target="_blank">/GoldWave.rar</a></u><br />
+</u>
+</p>
+<p>
+	<u><u><a href="/WinHex.exe" target="_blank">/WinHex.exe</a></u><br />
+</u>
+</p>
+
+<p>
+	<u><u><a href="/BCompare比对工具.exe" target="_blank">/BCompare比对工具.exe</a></u><br />
+</u>
+</p><p>
+	<u><u><a href="/BCD_EDIT启动菜单编辑器.7z" target="_blank">/BCD_EDIT启动菜单编辑器.7z</a></u><br />
+</u>
+</p>
+<p>
+	<u><u><a href="/BootSectGui引导扇区修复工具.exe" target="_blank">/BootSectGui引导扇区修复工具.exe</a></u><br />
+</u>
+</p>
+
+<p>
+	<u><u><a href="/CGI-Ghost.exe" target="_blank">/CGI-Ghost.exe</a></u><br />
+</u>
+</p><p>
+	<u><u><a href="/chmeditor.exe" target="_blank">/chmeditor.exe</a></u><br />
+</u>
+</p><p>
+	<u><u><a href="/TEST" target="_blank">/TEST</a></u><br />
+</u>
+</p><p>
+	<u><u><a href="/TEST" target="_blank">/TEST</a></u><br />
+</u>
+</p><p>
+	<u><u><a href="/TEST" target="_blank">/TEST</a></u><br />
 </u>
 </p>
